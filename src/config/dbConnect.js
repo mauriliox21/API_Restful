9954775c38935230d765e43596991b5ebbd04189
Maurilio Rodrigues <mauriliox21@gmail.com>
2022-09-13 16:26:43 -0300

    Ocultar url de conexao com banco de dados

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb+srv://cursoAlura:147258@cursoalura.ji82k7f.mongodb.net/curso-alura")
+mongoose.connect("mongodb+srv://*****************************.mongodb.net/curso-alura")
 
 let db = mongoose.connection;
 
